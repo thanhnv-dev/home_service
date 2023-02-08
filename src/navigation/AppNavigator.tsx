99cd589623a702826563f6a3724905c90daff538
thanhbv <thanhbv@newwave.com.vn>
2023-02-08 07:59:03 +0700

    update navigation

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,40 +1,80 @@
-import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Splash, Onboarding, LetIsIn, SignIn, SignUp} from '~/general/index';
+import * as React from 'react';
+import {LetIsIn, Onboarding, SignIn, SignUp, Splash} from '~/general/index';
 import CustomerStack from './CustomerStack';
+import {useSelector} from 'react-redux';
+import {getUser} from '~/redux/selectors/userSelector';
 
 const Stack = createNativeStackNavigator();
 const AppNaviagtor = () => {
-  const OnboardingStack = () => {
-    return (
-      <Stack.Navigator
-        screenOptions={{headerShown: false, gestureEnabled: false}}>
-        <Stack.Screen name="Splash" component={Splash} />
-        <Stack.Screen name="Onboarding" component={Onboarding} />
-      </Stack.Navigator>
-    );
-  };
+    const userProfile = useSelector(getUser);
+    const OnboardingStack = () => {
+        return (
+            <Stack.Navigator
+                screenOptions={{headerShown: false, gestureEnabled: false}}>
+                <Stack.Screen name="Splash" component={Splash} />
+                <Stack.Screen name="Onboarding" component={Onboarding} />
+            </Stack.Navigator>
+        );
+    };
 
-  const AuthStack = () => {
-    return (
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="LetIsIn" component={LetIsIn} />
-        <Stack.Screen name="SignIn" component={SignIn} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-      </Stack.Navigator>
-    );
-  };
+    const AuthStack = () => {
+        return (
+            <Stack.Navigator screenOptions={{headerShown: false}}>
+                <Stack.Screen name="LetIsIn" component={LetIsIn} />
+                <Stack.Screen name="SignIn" component={SignIn} />
+                <Stack.Screen name="SignUp" component={SignUp} />
+            </Stack.Navigator>
+        );
+    };
 
-  return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="OnboardingStack" component={OnboardingStack} />
-        <Stack.Screen name="AuthStack" component={AuthStack} />
-        <Stack.Screen name="CustomerStack" component={CustomerStack} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    const PublicStack = () => {
+        return (
+            <Stack.Navigator screenOptions={{headerShown: false}}>
+                <Stack.Screen
+                    name="OnboardingStack"
+                    component={OnboardingStack}
+                />
+                <Stack.Screen name="AuthStack" component={AuthStack} />
+            </Stack.Navigator>
+        );
+    };
+    switch (userProfile.type) {
+        case 'CUSTOMER':
+            return (
+                <NavigationContainer>
+                    <Stack.Navigator screenOptions={{headerShown: false}}>
+                        <Stack.Screen
+                            name="CustomerStack"
+                            component={CustomerStack}
+                        />
+                    </Stack.Navigator>
+                </NavigationContainer>
+            );
+        case 'PROVIDER':
+            return (
+                <NavigationContainer>
+                    <Stack.Navigator screenOptions={{headerShown: false}}>
+                        <Stack.Screen
+                            name="ProviderStack"
+                            component={ProviderStack}
+                        />
+                    </Stack.Navigator>
+                </NavigationContainer>
+            );
+        default:
+            return (
+                <NavigationContainer>
+                    <Stack.Navigator screenOptions={{headerShown: false}}>
+                        <Stack.Screen
+                            name="PublicStack"
+                            component={PublicStack}
+                        />
+                    </Stack.Navigator>
+                </NavigationContainer>
+            );
+    }
 };
 
 export default AppNaviagtor;
